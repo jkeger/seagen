87e2fd054cbe7ac2bb9a9e2d264c4c21e5e6df33
Josh Borrow <joshua.borrow@durham.ac.uk>
2018-03-03 09:14:06 +0000

    Added conversion helper functions and their relevant tests

---
+++ seagen/helper.py
@@ -0,0 +1,61 @@
+"""
+Helper functions for the seagen project.
+"""
+
+import numpy as np
+
+from typing import Tuple
+
+
+class InvalidCoordinate(Exception):
+    pass
+
+
+def check_valid_polar(r: np.ndarray, theta: np.ndarray, phi: np.ndarray) -> bool:
+    """
+    Check if these are valid polar co-ordinates; i.e.
+
+        + r \in [0, inf)
+        + theta \in [0, 2pi)
+        + phi \in [0, pi]
+
+    If not, it raises an InvalidCoordinate exception.
+    """
+    
+    if not ((min(r) >= 0.) and (np.isfinite(r).all())):
+        raise InvalidCoordinate(
+            "Your r values are not bounded between 0 and infinity"
+        )
+
+    if not ((min(theta) >= 0.) and (max(theta) < 2 * np.pi)):
+        raise InvalidCoordinate(
+            "Your theta values are not bounded between 0 and 2 pi"
+        )
+    
+    if not ((min(phi) >= 0.) and (max(phi) <= np.pi)):
+        raise InvalidCoordinate(
+            "Your phi values are not bounded between 0 and pi"
+        )
+
+    return True
+
+
+def polar_to_cartesian(
+        r: float,
+        theta:float,
+        phi: float
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Converts spherical polar to cartesian.
+    """
+
+    if check_valid_polar(r, theta, phi):
+        x = r * np.cos(theta) * np.sin(phi)
+        y = r * np.sin(theta) * np.sin(phi)
+        z = r * np.cos(phi)
+
+        return x, y, z
+    
+    else:
+        return None, None, None
+
---
+++ tests/test_helper.py
@@ -0,0 +1,32 @@
+"""
+Tests the helper functions
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d.axes3d as axes3d
+
+from seagen.helper import polar_to_cartesian
+
+
+def test_polar_to_cartesian():
+    phi = np.random.rand(1000) * np.pi
+    theta = np.random.rand(1000) * np.pi * 2
+
+    r = np.array([1])
+
+    x, y, z = polar_to_cartesian(r, theta, phi)
+
+    # Here we check if r is conserved!
+    new_r = np.sqrt(x*x + y*y + z*z)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1, projection="3d")
+
+    plot = ax.scatter(x, y, z)
+
+    plt.savefig("test_polar_to_cartesian.png")
+
+
+    assert np.isclose(r, new_r, 1e-9).all()
+
