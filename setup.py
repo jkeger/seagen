481a54b3eba9f46bd243cc3d0597a4bc64de5212
Jacob Kegerreis <jacob.kegerreis@durham.ac.uk>
2020-07-05 09:46:06 +0100

    Update package info

--- setup.py
+++ setup.py
@@ -6,16 +6,18 @@ with open("README.md", "r") as f:
 setuptools.setup(
     name="seagen",
     packages=setuptools.find_packages(),
-    version="1.3.2",
+    version="1.4",
     description="Stretched Equal Area Generator",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Jacob Kegerreis",
     author_email="jacob.kegerreis@durham.ac.uk",
     url="https://github.com/jkeger/seagen",
-    download_url="https://github.com/jkeger/seagen/archive/1.3.2.tar.gz",
+    download_url="https://github.com/jkeger/seagen/archive/1.4.tar.gz",
     project_urls={
         "Paper": "https://doi.org/10.1093/mnras/stz1606",
+        "Group": "http://icc.dur.ac.uk/giant_impacts",
+        "WoMa": "https://github.com/srbonilla/WoMa",
     },
     license="GNU GPLv3+",
     py_modules=["seagen"],
@@ -26,5 +28,5 @@ setuptools.setup(
         ],
     python_requires=">=3",
     install_requires=["numpy"],
-    keywords=["particle arrangement density sphere shell SPH"],
+    keywords=["particle arrangement density sphere shell SPH initial conditions"],
 )
